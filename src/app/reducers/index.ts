246c314137c37280fadd9195553e3ccd35ae7788
Elias Rosales <eliasrosales88@gmgmail.com>
2019-09-01 22:56:50 +0200

    Implementing authReducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,13 +6,42 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import { User } from '../model/user.model';
+import { AuthActionTypes } from '../auth/auth.actions';
+
+type AuthState = {
+  loggedIn: boolean,
+  user: User
+}
+
+// Function to be executed by the reducer before injecting to the appState
+function authReducer( state: AuthState, action): AuthState {
+  switch(action.type){
+    
+    case AuthActionTypes.LoginAction:
+      return {
+        loggedIn: true,
+        user: action.payload.user
+      }
+    
+    default:
+      return state
+  }
+}
+
 
 //Data structure of the AppState
 export interface AppState {
 
+  // auth is the first property inyected in the appState
+  auth: AuthState,
+  // courses: CoursesState,
+  // lessons: LessonsState
 }
 
 export const reducers: ActionReducerMap<AppState> = {
+ 
+  auth: authReducer
 
 };
 
