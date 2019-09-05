a6171cfd0c433ae5db82488eed40419eebdffcfc
Elias Rosales <eliasrosales88@gmgmail.com>
2019-09-05 02:29:05 +0200

    Adding ngrx effects

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,13 +1,43 @@
 import { Injectable } from '@angular/core';
-import { Actions, Effect } from '@ngrx/effects';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Login, AuthActionTypes, Logout } from './auth.actions';
+import { tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { defer, of } from 'rxjs';
 
 
 
 @Injectable()
 export class AuthEffects {
+  
+  @Effect({dispatch: false})
+  login$ = this.actions$.pipe(
+    // identificamos el login action con ofType
+    ofType<Login>(AuthActionTypes.LoginAction),
+    tap(action => localStorage.setItem('user', JSON.stringify(action.payload.user)))
+  );
+  
+  @Effect({dispatch: false})
+  logout$ = this.actions$.pipe(
+    ofType<Logout>(AuthActionTypes.LogoutAction),
+    tap(() =>  {
+      localStorage.removeItem('user');
+      this.router.navigateByUrl('/login');
+    
+    })
+  );
 
+  @Effect()
+  init$ = defer(() => { // defer will create an observable
+    const userData = localStorage.getItem("user");
+    if (userData) {
+      return of(new Login(JSON.parse(userData)))
+    }
+    else {
+      return of(new Logout());
+    }
 
-
-  constructor(private actions$: Actions) {}
+  });
+  constructor(private actions$: Actions, private router: Router) {}
 
 }
